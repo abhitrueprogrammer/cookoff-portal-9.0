48c0df1294f787482825bee3e87666c3b7ede461
Harshit <kashyapharshit50@gmail.com>
2024-09-14 21:57:11 +0530

    changed as per comments

--- src/app/test-complete/page.tsx
+++ src/app/test-complete/page.tsx
@@ -1,51 +1,39 @@
 "use client";
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
-
 import check from "@/assests/images/check.png";
 import ChangeDevice from '../../components/changeDevice/ChangeDevice';
 import NavBar from '../../components/navBar/NavBar';
-import "./style.css";
+import styles from "./test-complete.module.css";
 
 export default function TestComplete() {
-    const [isSmall, setIsSmall] = useState(false);
-    useEffect(() => {
-        const mediaQuery = window.matchMedia('(max-width: 1024px)');
-        setIsSmall(mediaQuery.matches)
-        const handleMediaChange = (event: MediaQueryListEvent) => {
-            setIsSmall(event.matches);
-        };
-
-
-        mediaQuery.addEventListener('change', handleMediaChange);
-
-        return () => mediaQuery.removeEventListener('change', handleMediaChange);
-    }, [])
     return (
-        <>{isSmall ? <ChangeDevice /> :
-            <>
+        <>
+            {/* Mobile View */}
+            <div className="block lg:hidden">
+                <ChangeDevice />
+            </div>
+
+            {/* Desktop View */}
+            <div className="hidden lg:block h-screen bg-dark">
                 <NavBar />
-                <div className='bg-[#131313] h-screen'>
-
-                    <div className="bg-[#131313] flex justify-center items-center flex-col h-[85vh]">
-                        <div className=''>
-                            <div className='rounded-full border-[3px] border-[#F14A16]'>
-                                <div className='bg-[#4D4D4D80] border-[6px] border-[#131313] rounded-full '>
-                                    <Image src={check} alt='check' width={100} height={100} className='check-img' />
-                                </div>
+                <div className='bg-dark flex justify-center items-center flex-col h-[85vh]'>
+                    <div className=''>
+                        <div className='rounded-full border-[3px] border-accent'>
+                            <div className='bg-lightGray border-[6px] border-dark rounded-full '>
+                                <Image src={check} alt='check' width={100} height={100} className={styles.checkImg} />
                             </div>
-
-                        </div>
-                        <div className='text-[#E4D8C6] text-xl mt-[30px]'>
-                            You have completed the test. Stay tuned for the Results!
-                        </div>
-                        <div className='mt-[50px]'>
-                            <button className='text-[#E4D8C6] border-[#F14A16] border-2 rounded-md px-6 py-2'>
-                                Back To Dashboard
-                            </button>
                         </div>
                     </div>
-                </div></>}
+                    <div className='text-cream text-xl mt-[30px]'>
+                        You have completed the test. Stay tuned for the Results!
+                    </div>
+                    <div className='mt-[50px]'>
+                        <button className='text-cream border-accent border-2 rounded-md px-6 py-2'>
+                            Back To Dashboard
+                        </button>
+                    </div>
+                </div>
+            </div>
         </>
-    )
+    );
 }
--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import acc from "@/assests/images/acc.png"
 import Image from 'next/image';
+import styles from "./navBar.module.css"
 
 export default function NavBar() {
     return (
         <div>
-            <div className="s-sling nav bg-black flex justify-around items-center border-b-[1px] ">
+            <div className="s-sling nav bg-black flex justify-around items-center border-b-1 ">
                 <div className='px-4 py-1 text-[#F14A16] border-[1px] border-white text-lg flex items-center '>9:11:69</div>
                 <div className='text-white text-5xl my-3'>
                     <span className='text-[#B7AB98]'>cook</span> <span className='text-[#F14A16]'>off</span><span className='text-[#B7AB98]'> 9.0</span> </div>
                 <div className='flex text-[#B7AB98] justify-evenly'>
                     <button className='bg-[#1F1F1F] text-sm px-7'>logout</button>
-                    <Image src={acc} height={100} width={100} alt='account' layout='responsive' className='acc-img' />
+                    <Image src={acc} height={100} width={100} alt='account' layout='responsive' className={styles.accImg} />
                 </div>
             </div>
         </div>
