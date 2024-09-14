ee110ceaea79199bb316416810d6391d8ae260a2
Harshit <kashyapharshit50@gmail.com>
2024-09-14 21:24:30 +0530

    some routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 
-
-import TestComplete from "@/components/testComplete/TestComplete";
 export default function HomePage() {
   return (
     <main className="">
-      <TestComplete/>
+
     </main>
   );
 }
