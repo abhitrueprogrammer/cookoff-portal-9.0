3eaa3bd47fb14ad22ba3998f4bf739bcadb2d4b9
Harshit <kashyapharshit50@gmail.com>
2024-09-15 23:56:42 +0530

    chote changes

--- src/components/changeDevice/ChangeDevice.tsx
+++ src/components/changeDevice/ChangeDevice.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ChangeDevice() {
   return (
-    <div className='s-sling flex justify-center items-center h-screen bg-[#131313] text-[#F14A16] text-4xl text-center'>
+    <div className='s-sling flex justify-center items-center h-screen bg-dark text-accent text-4xl text-center'>
       PLEASE SWICTH TO DESKTOP
     </div>
   )
